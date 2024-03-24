475cdb319d3ba6ebfd2109438d0d2f174860c9df
Georgegitahi <petergeorgembugua724@gmail.com>
2024-03-24 21:34:54 +0300

    Lab complete

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+//Multiply function
+// const multiply=(num1,num2)=>{
+//     return num1 * num2;
+// };
+// let resultMultiply= multiply(31*2);
+// console.log(resultMultiply =62);
+// //62
 
-// Write your code here
+
+// Challenge 1: Multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+// Challenge 2: Random
+let random = Math.floor(Math.random() * 100) + 1;
+
+// Challenge 3: Mod
+let num3 = 14; // Change num3 value to 14
+let num4 = 5;  // Change num4 value to 5
+let mod = num3 % num4;
+
+// Challenge 4: Max
+let max = Math.max(1, 2, 3, 4, 5, 10, 15, 20, 15);
